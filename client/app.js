965d5d61ae7e2385dc814085e3e36f9bfe2de81b
ASA <karoteglathe@gmail.com>
2017-05-22 13:55:41 +0800

    Why:
    * add app root
    
    This change addresses the need by:
    
    *

--- client/app.js
+++ client/app.js
@@ -1,5 +1,11 @@
-document.body.innerHTML = 'Hello World!'
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import Board from './pages/board.js'
 
+ReactDOM.render(
+	<Board />,
+	document.getElementById('root')
+);
 
 if (module.hot) {
 	module.hot.accept();
---
+++ client/pages/board.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Board extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div>Hello World!YO~~</div>
+		)
+	}
+}
+
+export default Board;
