60d9ca2ab780bdcd511f3fa651b7923034a6809b
ASA <karoteglathe@gmail.com>
2017-05-31 13:23:12 +0800

    update webpack config for scss

--- webpack.config.js
+++ webpack.config.js
@@ -29,10 +29,13 @@ const config = {
       },
       {
         test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader']
-        })
+        use: [{
+          loader: "style-loader" // creates style nodes from JS strings
+        }, {
+          loader: "css-loader" // translates CSS into CommonJS
+        }, {
+          loader: "sass-loader" // compiles Sass to CSS
+        }]
       },
       {
         test: /\.css$/,
