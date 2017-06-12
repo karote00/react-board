f7d6664bd85170fae80eddd926ea519d09d1853d
ASA <karoteglathe@gmail.com>
2017-06-12 13:43:25 +0800

    add sessionStorage to data control and add addColumn api

--- client/services/index.js
+++ client/services/index.js
@@ -3,15 +3,43 @@ const TIMEOUT = 1000;
 const api = {
 	getToggleEditItem(item) {
 		return new Promise((resolve, reject) => {
-			setTimeout(() => resolve(item), TIMEOUT)
+			setTimeout(() => resolve(item), TIMEOUT);
 		});
 	},
 	getColumns() {
+		const self = this;
 		return new Promise((resolve, reject) => {
-			fetch('../mock/column.json', {method: 'get'})
-				.then(res => resolve(res))
-				.catch(err => reject(err));
+			let columns = this.getStorage('columns');
+
+			if (!columns || Object.keys(columns).length == 0) {
+				fetch('./client/mock/columns.json', {method: 'get'})
+					.then(res => {
+						return res.json();
+					})
+					.then(r => {
+						self.setStorage('columns', JSON.stringify(r));
+						return resolve(r)
+					})
+					.catch(err => reject(err));
+			} else {
+				resolve(columns);
+			}
 		})
+	},
+	addColumn(column) {
+		return new Promise((resolve, reject) => {
+			let columns = this.getStorage('columns');
+			columns.push(column);
+			this.setStorage('columns', JSON.stringify(columns));
+			setTimeout(() => resolve(column), TIMEOUT);
+		})
+	},
+	getStorage(key) {
+		let items = sessionStorage.getItem(key);
+		return items && JSON.parse(items) || null;
+	},
+	setStorage(key, value) {
+		sessionStorage.setItem(key, value);
 	}
 };
 
