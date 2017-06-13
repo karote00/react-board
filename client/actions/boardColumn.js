01f876a368647ef7863ad6b601b5597256da1dc4
ASA <karoteglathe@gmail.com>
2017-06-13 16:35:53 +0800

    add getColumns request, success and failed action

--- client/actions/boardColumn.js
+++ client/actions/boardColumn.js
@@ -32,6 +32,35 @@ export const getColumns = (status, columns) => {
 	}
 }
 
+// next: (res) => res.json()
+// 								.then(data => {
+// 									self.setStorage('columns', JSON.stringify(data));
+// 									dispatch(getColumns('SUCCESS', data))
+// 								}),
+// 				error: (err) => dispatch(getColumns('FAILED')),
+// 				completed: () => {}
+
+export const getColumnsRequest = () => {
+	return {
+		type: GET_COLUMNS.REQUEST,
+	}
+};
+
+export const getColumnsSuccess = (data) => {
+	return {
+		type: GET_COLUMNS.SUCCESS,
+		payload: {
+			columns: data
+		}
+	}
+}
+
+export const getColumnsFailed = () => {
+	return {
+		type: GET_COLUMNS.FAILED
+	}
+}
+
 export const addColumn = (status, column) => {
 	return {
 		type: ADD_COLUMN[status],
