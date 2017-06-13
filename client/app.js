0ce6ed93b2423756598abc8139d0c857e9c35ec8
ASA <karoteglathe@gmail.com>
2017-06-13 22:20:41 +0800

    add logger and crashReporter middleware

--- client/app.js
+++ client/app.js
@@ -10,9 +10,31 @@ import mySaga from './sagas';
 
 // const sagaMiddleware = createSagaMiddleware();
 
+const logger = store => next => action => {
+  console.log('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  return result
+}
+
+const crashReporter = store => next => action => {
+  try {
+    return next(action)
+  } catch (err) {
+    console.error('Caught an exception!', err)
+    Raven.captureException(err, {
+      extra: {
+        action,
+        state: store.getState()
+      }
+    })
+    throw err
+  }
+}
+
 const store = createStore(
   reducer,
-  applyMiddleware()
+  applyMiddleware(logger, crashReporter)
   // applyMiddleware(sagaMiddleware)
 );
 
