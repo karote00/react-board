d62126891abb289b75fb184ae9db61ec20181997
ASA <karoteglathe@gmail.com>
2017-06-20 01:22:49 +0800

    check if PRODUCTION mode

--- client/services/index.js
+++ client/services/index.js
@@ -1,8 +1,8 @@
 import Rx from 'rxjs';
 import { getColumns, addColumn } from '../actions/boardColumn';
 
+const PRODUCTION = ENV.status === 'production'? true: false;
 const TIMEOUT = 1000;
-
 const api = {
 	getToggleEditItem(item) {
 		return new Promise((resolve, reject) => {
@@ -10,8 +10,25 @@ const api = {
 		});
 	},
 	getColumns(observer) {
-		const result = Rx.Observable.fromPromise(fetch('./client/mock/columns.json'));
-		result.subscribe(observer)
+		let result;
+		if (PRODUCTION) {
+			result = Rx.Observable.of([
+				{
+					"id": 1,
+					"name": "Features"
+				},
+				{
+					"id": 2,
+					"name": "Todo"
+				}
+			]);
+		} else {
+			const apiUrl = './client/mock/columns.json';
+			result = Rx.Observable.fromPromise(fetch(apiUrl))
+								.concatMap((res) => res.json());
+		}
+
+		result.subscribe(observer);
 	},
 	addColumn(column) {
 		return new Promise((resolve, reject) => {
