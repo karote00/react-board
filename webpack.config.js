c52de684e237fa80242c96cef1470c3ccaef2476
ASA <karoteglathe@gmail.com>
2017-06-20 01:21:38 +0800

    fix bundle

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,28 @@ const webpack             = require('webpack');
 const path                = require('path');
 const ExtractTextPlugin   = require('extract-text-webpack-plugin');
 const HTMLWebpackPlugin   = require('html-webpack-plugin');
+const HtmlPluginRemove    = require('html-webpack-plugin-remove');
+const CleanWebpackPlugin  = require('clean-webpack-plugin');
 const SWPrecachePlugin    = require('sw-precache-webpack-plugin');
 const extractCSS          = new ExtractTextPlugin('stylesheets/[name].css');
 const hotMiddlewareScript = 'webpack-hot-middleware/client';
-const publicPath          = path.resolve(__dirname, './dist');
-const envConfig           = require('./config/env.js');
+const envConfig           = require('./config/envConfig.js');
 const isProd              = envConfig.isProd;
-const bundlePath          = isProd? '/dist': '/client';
+const devtool             = isProd? 'source-map': 'cheap-module-eval-source-map';
+const bundlePath          = './dist';
+const publicPath          = path.resolve(__dirname, bundlePath);
+
+const pathsToClean = ['dist'];
+let cleanOptions = {
+  root:     '/',
+  // exclude:  ['shared.js'],
+  verbose:  true,
+  // dry:      false
+};
+
+let envToBeInjected = {
+  status: isProd? "production": "dev"
+};
 
 const config = {
   context: __dirname,
@@ -18,7 +33,7 @@ const config = {
       './client/app.js'
     ],
   },
-  devtool: isProd? 'source-map': 'cheap-module-eval-source-map',
+  devtool: devtool,
   // target: 'node',
   module: {
     rules: [
@@ -48,40 +63,49 @@ const config = {
   },
   output: {
     filename: 'bundle.js',
-    publicPath: path.resolve(__dirname, bundlePath),
-    path: bundlePath
+    publicPath: path.resolve(__dirname, '/client'),
+    path: '/client'
   },
   plugins: [
+    new webpack.DefinePlugin({
+      ENV: JSON.stringify(envToBeInjected)
+    }),
     new ExtractTextPlugin({
       filename: 'styles.[hash].css',
       allChunks: true
     }),
-    new HTMLWebpackPlugin(),
-    // new HTMLWebpackPlugin({
-    //   template: 'client/index.html'
-    // }),
+    // new HTMLWebpackPlugin(),
+    // new HtmlPluginRemove(/<script.*?src="\/client\..*?\.js".*?<\/script>/),
+    new HtmlPluginRemove(/<script.*?src="\/client\/bundle.js".*?<\/script>/),
+    new HTMLWebpackPlugin({
+      template: 'client/index.html'
+    }),
     new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoEmitOnErrorsPlugin()
+    new webpack.NoEmitOnErrorsPlugin(),
+    new CleanWebpackPlugin(pathsToClean, cleanOptions)
   ]
 };
 
 if (process.env.NODE_ENV === 'production') {
-  config.output.publicPath = './';
+  config.output.publicPath = '/dist';
+  config.output.filename = '[name].[hash].js'
+  config.output.path = publicPath;
 
   config.plugins.push(
     // minify JS
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
+    // new webpack.optimize.UglifyJsPlugin({
+    //   compress: {
+    //     warnings: false
+    //   }
+    // })
+    // }),
     // auto generate service worker
-    new SWPrecachePlugin({
-      cacheId: 'vue-hn',
-      filename: 'service-worker.js',
-      dontCacheBustUrlsMatching: /./,
-      staticFileGlobsIgnorePatterns: [/index\.html$/, /\.map$/]
-    })
+    // new SWPrecachePlugin({
+    //   cacheId: 'vue-hn',
+    //   filename: 'service-worker.js',
+    //   dontCacheBustUrlsMatching: /./,
+    //   staticFileGlobsIgnorePatterns: [/index\.html$/, /\.map$/]
+    // })
   )
 }
 
